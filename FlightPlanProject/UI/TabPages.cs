f82f2fe4689ca4aeb5553627ed7b05c06fae0926
Steve Ratts <50781429+schlosrat@users.noreply.github.com>
2023-05-06 12:21:13 -0400

    Defined Icon parameter for OwnshipManeuversPage, TargetPage, InterplanetaryPage, and MoonPage. Added new ResonantOrbitPage

--- FlightPlanProject/UI/TabPages.cs
+++ FlightPlanProject/UI/TabPages.cs
@@ -3,6 +3,9 @@
 using KSP.Sim.impl;
 
 using FlightPlan.KTools.UI;
+using SpaceWarp.API.Assets;
+
+using UnityEngine;
 
 namespace FlightPlan;
 
@@ -22,6 +25,8 @@ public class BasePageContent : PageContent
 
     public virtual string Name => throw new NotImplementedException();
 
+    public virtual GUIContent Icon => throw new NotImplementedException();
+
     public bool isRunning => false;
 
 
@@ -39,6 +44,10 @@ public class BasePageContent : PageContent
 public class OwnshipManeuversPage : BasePageContent
 {
     public override string Name => "Own Orbit";
+
+    readonly Texture2D tabIcon = AssetManager.GetAsset<Texture2D>($"{FlightPlanPlugin.Instance.SpaceWarpMetadata.ModID}/images/OwnshipManeuver_50.png");
+
+    public override GUIContent Icon => new(tabIcon, "Ownship Maneuvers");
 
     public override bool isActive => true;
 
@@ -77,16 +86,20 @@ public class TargetPage : BasePageContent
 {
     public override string Name => "Target";
 
+    readonly Texture2D tabIcon = AssetManager.GetAsset<Texture2D>($"{FlightPlanPlugin.Instance.SpaceWarpMetadata.ModID}/images/TargetRelManeuver_50.png");
+
+    public override GUIContent Icon => new(tabIcon, "Target Relative Maneuvers");
+
     public override bool isActive
     {
-        get => plugin.currentTarget != null  // If the activeVessel and the currentTarget are both orbiting the same body
-            && plugin.currentTarget.Orbit != null // No maneuvers relative to a star
-            && plugin.currentTarget.Orbit.referenceBody.Name == referenceBody.Name;
+        get => plugin.currentTarget != null  // If the is a target
+            && plugin.currentTarget.Orbit != null // And the target is not a star
+            && plugin.currentTarget.Orbit.referenceBody.Name == referenceBody.Name; // If the activeVessel and the currentTarget are both orbiting the same body
     }
 
     public override void onGUI()
     {
-        FPStyles.DrawSectionHeader("Maneuvers Relative to Target");
+        FPStyles.DrawSectionHeader("Target Relative Maneuvers");
 
         BurnTimeOption.Instance.OptionSelectionGUI();
 
@@ -106,6 +119,10 @@ public class InterplanetaryPage : BasePageContent
 {
     public override string Name => "Target";
 
+    readonly Texture2D tabIcon = AssetManager.GetAsset<Texture2D>($"{FlightPlanPlugin.Instance.SpaceWarpMetadata.ModID}/images/TargetRelManeuver_50.png");
+
+    public override GUIContent Icon => new(tabIcon, "Orbital Transfer Maneuvers");
+
     public override bool isActive
     {
         get => plugin.currentTarget != null // If the activeVessel is orbiting a planet and the current target is not the body the active vessel is orbiting
@@ -117,7 +134,7 @@ public class InterplanetaryPage : BasePageContent
     }
     public override void onGUI()
     {
-        FPStyles.DrawSectionHeader("Interplanetary Maneuvers");
+        FPStyles.DrawSectionHeader("Orbital Transfer Maneuvers");
         BurnTimeOption.Instance.OptionSelectionGUI();
         main_ui.DrawToggleButton("Interplanetary Transfer", ManeuverType.planetaryXfer);
     }
@@ -128,6 +145,10 @@ public class MoonPage : BasePageContent
 {
     public override string Name => "Moon";
 
+    readonly Texture2D tabIcon = AssetManager.GetAsset<Texture2D>($"{FlightPlanPlugin.Instance.SpaceWarpMetadata.ModID}/images/TargetRelManeuver_50.png");
+
+    public override GUIContent Icon => new(tabIcon, "Orbital Transfer Maneuvers");
+
     public override bool isActive
     {
         get => !referenceBody.IsStar // not orbiting a star
@@ -137,10 +158,52 @@ public class MoonPage : BasePageContent
     }
     public override void onGUI()
     {
-        FPStyles.DrawSectionHeader("Moon Specific Maneuvers");
+        FPStyles.DrawSectionHeader("Orbital Transfer Maneuvers");
 
         var parentPlanet = referenceBody.Orbit.referenceBody;
         FPSettings.mr_altitude_km = main_ui.DrawToggleButtonWithTextField("Moon Return", ManeuverType.moonReturn, FPSettings.mr_altitude_km, "km");
         main_ui.targetMRPeR = FPSettings.mr_altitude_km * 1000 + parentPlanet.radius;
     }
 }
+
+public class ResonantOrbitPage : BasePageContent
+{
+    public override string Name => "Resonant Orbit";
+
+    readonly Texture2D tabIcon = AssetManager.GetAsset<Texture2D>($"{FlightPlanPlugin.Instance.SpaceWarpMetadata.ModID}/images/ResonantOrbit_50.png");
+
+    public override GUIContent Icon => new(tabIcon, "Resonant Orbit Maneuvers");
+
+    public override bool isActive => true;
+
+    public override void onGUI()
+    {
+        FPStyles.DrawSectionHeader("Resonant Orbit Maneuvers");
+        BurnTimeOption.Instance.OptionSelectionGUI();
+        main_ui.DrawToggleButton("Circularize", ManeuverType.circularize);
+        // GUILayout.EndHorizontal();
+
+        FPSettings.pe_altitude_km = main_ui.DrawToggleButtonWithTextField("New Pe", ManeuverType.newPe, FPSettings.pe_altitude_km, "km");
+        main_ui.targetPeR = FPSettings.pe_altitude_km * 1000 + referenceBody.radius;
+
+        if (main_ui.orbit.eccentricity < 1)
+        {
+            FPSettings.ap_altitude_km = main_ui.DrawToggleButtonWithTextField("New Ap", ManeuverType.newAp, FPSettings.ap_altitude_km, "km");
+            main_ui.targetApR = FPSettings.ap_altitude_km * 1000 + referenceBody.radius;
+            main_ui.DrawToggleButton("New Pe & Ap", ManeuverType.newPeAp);
+        }
+
+        FPSettings.target_inc_deg = main_ui.DrawToggleButtonWithTextField("New Inclination", ManeuverType.newInc, FPSettings.target_inc_deg, "°");
+
+        if (plugin.experimental.Value)
+        {
+            FPSettings.target_lan_deg = main_ui.DrawToggleButtonWithTextField("New LAN", ManeuverType.newLAN, FPSettings.target_lan_deg, "°");
+
+            // FPSettings.target_node_long_deg = DrawToggleButtonWithTextField("New Node Longitude", ref newNodeLon, FPSettings.target_node_long_deg, "°");
+        }
+
+        FPSettings.target_sma_km = main_ui.DrawToggleButtonWithTextField("New SMA", ManeuverType.newSMA, FPSettings.target_sma_km, "km");
+        main_ui.targetSMA = FPSettings.target_sma_km * 1000 + referenceBody.radius;
+    }
+}
+
