75e9a3bd0c45a45cf689a1773c84c6e8c66d6263
Steve Ratts <50781429+schlosrat@users.noreply.github.com>
2024-02-10 10:08:35 -0500

    Aligned with MJ

--- src/FlightPlan/MJ/Vector6.cs
+++ src/FlightPlan/MJ/Vector6.cs
@@ -13,7 +13,7 @@ namespace MuMech
     {
         public Vector3d positive = Vector3d.zero, negative = Vector3d.zero;
 
-        public enum Direction { FORWARD = 0, BACK = 1, UP = 2, DOWN = 3, RIGHT = 4, LEFT = 5 };
+        public enum Direction { FORWARD = 0, BACK = 1, UP = 2, DOWN = 3, RIGHT = 4, LEFT = 5 }
 
         public static readonly Vector3d[] directions = { Vector3d.forward, Vector3d.back, Vector3d.up, Vector3d.down, Vector3d.right, Vector3d.left };
 
